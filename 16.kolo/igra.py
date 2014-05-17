1d4f8f78c708c8dfea6346f8bed32ed9654b1179
Neven Sajko <nsajko@gmail.com>
2014-05-17 02:42:14 +0200

    Heavily modify igra.py
    Reimplement main (hole-counting) procedure as a recursive function.
    Also set 'debug' to False

--- 16.kolo/igra.py
+++ 16.kolo/igra.py
@@ -2,14 +2,19 @@
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 # Neven Sajko
 
-debug = True
+debug = False
 
 
-def pazi_rupe(polje):
+def kolika_rupa(polje, koraka=1):
     '''Provjerava kolika je rupa prije danog polja koristeći
-    skup polja-rupa 'rupe', te postavlja 'koraci' na broj dovoljan za
+    skup polja-rupa 'rupe', te vraća broj [1-6] koraka dovoljan za
     preskakanje te rupe.
     '''
+    if polje in rupe:
+        return(kolika_rupa(polje - 1, koraka + 1))
+    else:
+        return(koraka)
+
 rupe = {int(input()) for rupa in range(int(input()))}
 if debug:
     print(rupe)
@@ -22,22 +27,7 @@ for polje in range(1, 101):
     if debug:
         print(polje, koraci, potezi)
     if koraci == 6:
-        if polje in rupe:
-            if polje - 1 in rupe:
-                if polje - 2 in rupe:
-                    if polje - 3 in rupe:
-                        if polje - 4 in rupe:
-                            koraci = 6
-                        else:
-                            koraci = 5
-                    else:
-                        koraci = 4
-                else:
-                    koraci = 3
-            else:
-                koraci = 2
-        else:
-            koraci = 1
+        koraci = kolika_rupa(polje)
         potezi += 1
         continue
     koraci += 1
