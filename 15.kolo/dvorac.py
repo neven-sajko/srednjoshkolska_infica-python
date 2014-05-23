a46e409e465202ca84cd6900b824d079f3a4df94
Neven Sajko <nsajko@gmail.com>
2014-05-23 21:44:35 +0200

    Modify the main loop in 15.liga/dvorac.py
    Turn before and after circling and also don't hold the pen down while
    going through the circle.
    Also add a comment explaining the turtle.degrees usage.
    Note: Most of the loop could be cut out if a different turtle.circle
    extent would be used.

--- 15.kolo/dvorac.py
+++ 15.kolo/dvorac.py
@@ -2,13 +2,18 @@
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 # Neven Sajko
 
-from turtle import degrees, forward, circle  # , left, pen
+from turtle import degrees, forward, circle, left, pen
 
 (n, d, r) = input().split()
 (n, d, r) = (int(n), int(d), int(r))
 
-degrees(n)
+degrees(n)  # 'n' is the full circle.
 
 for unused_count in range(n):
-    forward(d-2*r)
+    forward(d-4*r)
+    pen(pendown=False)
+    forward(2*r)
+    pen(pendown=True)
+    left(n/4)
     circle(r, 2*n-1)
+    left(-n/4)
